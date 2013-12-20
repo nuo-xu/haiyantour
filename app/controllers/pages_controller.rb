10167c907b78d6b0b8dc49c154893db18535fa9b
nuo-xu <stenoxu@gmail.com>
2013-12-20 02:34:58 -0500

    added the local weather column in the page using yahoo weather api

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,32 @@
+require 'net/http'
+require 'json'
+
 class PagesController < ApplicationController
+  helper_method :weather
+  ROOT = "http://query.yahooapis.com/v1/public/yql"
+  
+  def weather
+    url = ROOT + "?q=select%20*%20from%20weather.forecast%20"
+    url += "where%20woeid%3D2131595%20and%20u%3D'c'&format=json"
+    doc = get_response url
+    doc
+  end
+
+  def get_response url
+    begin
+      response = Net::HTTP.get_response(URI.parse url).body.to_s
+    rescue => e
+      raise "Failed to get weather [url=#{url}, e=#{e}]."
+    end
+
+    response = JSON.parse(response)
+    if response.nil?
+      raise "Failed to get weather [url=#{url}]."
+    end
+    result = response["query"]["results"]["channel"]["item"]["description"]
+    result
+  end
+  
   def home
   end
 
