f03f6d177109b258efee921709936dd636269358
nuo-xu <stenoxu@gmail.com>
2013-12-30 18:41:42 -0500

    get rid of the fadein fadeout effects for the slide-page, just using simple show and hide

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -3,8 +3,8 @@ $('.page-slides img:gt(0)').hide();
 $(function(){
     $('.page-slides img:gt(0)').hide();
     setInterval(function(){
-      $('.page-slides :first-child').fadeOut()
-         .next('img').fadeIn()
+      $('.page-slides :first-child').hide()
+         .next('img').show()
          .end().appendTo('.page-slides');}, 
       6000);
 });
