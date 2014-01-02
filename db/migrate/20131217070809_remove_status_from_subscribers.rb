5f7312bc697556323c451080902cb68461c710f0
nuo-xu <stenoxu@gmail.com>
2014-01-01 19:39:43 -0500

    change the migration remove column from table subscribers back to the change function. since the down and up functions cause problem if someone clone this repository

--- db/migrate/20131217070809_remove_status_from_subscribers.rb
+++ db/migrate/20131217070809_remove_status_from_subscribers.rb
@@ -1,12 +1,5 @@
 class RemoveStatusFromSubscribers < ActiveRecord::Migration
-  # def change
-  #   remove_column :subscribers, :status, :boolean
-  # end
-  def down
-    remove_column :subscribers, :status
-  end
-
-  def up
-    add_column :subscribers, :status, :boolean
+  def change
+    remove_column :subscribers, :status, :boolean
   end
 end
