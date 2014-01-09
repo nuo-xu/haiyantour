44423d0e34d027e01a5790e028284a9e1bba40b0
nuo-xu <stenoxu@gmail.com>
2014-01-09 18:32:13 -0500

    add the function to stock the new contact object to our contacts table in database.

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,20 @@
+class ContactsController < ApplicationController
+  def create
+    @contact = Contact.new(contact_params)
+
+    respond_to do |format|
+      if @contact.save
+        format.html { redirect_to contact_url, notice: 'Thank you for submitting your letter. We will contact you shortly.' }
+      else
+        format.html { redirect_to root_url, notice: 'Had an issue for submitting!' }
+      end
+    end
+  end
+
+    # TODO: check the cases of different inputs parameters 
+    private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def contact_params
+      params.require(:contact).permit(:first_name, :last_name, :email, :telephone, :company, :kind, :content, :heard_about_us)
+    end
+end
